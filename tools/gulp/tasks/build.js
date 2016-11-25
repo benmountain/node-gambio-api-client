eaa9eb6c52998b9faa1fecf3decd7302b55c42e9
r.loyko <r.loyko@gambio.de>
2016-11-25 15:42:47 +0100

    Created build task.

--- tools/gulp/tasks/build.js
+++ tools/gulp/tasks/build.js
@@ -1,5 +1,5 @@
 module.exports = (gulp, callback) => {
   const sequence = require('run-sequence').use(gulp);
 
-  return sequence('build', 'watch', callback);
+  return sequence('clean', 'compile', callback);
 };
