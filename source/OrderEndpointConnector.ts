cc3cdd2dd45182a776e1df547bbd5d0ffa95baeb
rx2x <hello@rx2x.net>
2016-12-14 21:07:05 +0100

    Extended OrderEndpointConnector (work in progress).

--- source/OrderEndpointConnector.ts
+++ source/OrderEndpointConnector.ts
@@ -141,6 +141,36 @@ class OrderEndpointConnector extends AbstractEndpointConnector {
   }
 
   /**
+   * Deletes an order item property by its ID.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderItemId Order item ID.
+   * @param {Number} orderItemPropertyId Order item property ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order item ID parameter.
+   * @throws Will throw error on missing or invalid order item property ID parameter.
+   */
+  public deleteItemProperty(orderId: number, orderItemId: number, orderItemPropertyId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order item ID parameter.
+    if (!orderItemId || typeof orderItemId !== 'number') {
+      throw new Error('Missing or invalid order item ID');
+    }
+
+    // Check order item property ID parameter.
+    if (!orderItemPropertyId || typeof orderItemPropertyId !== 'number') {
+      throw new Error('Missing or invalid order item property ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.delete(`${this.getRoute()}/${orderId}/items/${orderItemId}/properties/${orderItemPropertyId}`);
+  }
+
+  /**
    * Deletes an order item by its ID.
    * @param {Number} orderId Order ID.
    * @param {Number} orderItemId Order item ID.
@@ -200,6 +230,270 @@ class OrderEndpointConnector extends AbstractEndpointConnector {
 
     // Perform request.
     return this.requestDispatcher.delete(`${this.getRoute()}/${id}`);
+  }
+
+  /**
+   * Returns an order history.
+   * @param {Number} id Order ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid ID parameter.
+   */
+  public getHistory(id: number): Promise<ResponseInterface> {
+    // Check ID parameter.
+    if (!id || typeof id !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${id}/history`);
+  }
+
+  /**
+   * Returns an order history by its ID.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderHistoryId Order history ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order history ID parameter.
+   */
+  public getHistoryById(orderId: number, orderHistoryId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order history ID parameter.
+    if (!orderHistoryId || typeof orderHistoryId !== 'number') {
+      throw new Error('Missing or invalid order history ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${orderId}/history/${orderHistoryId}`);
+  }
+
+  /**
+   * Returns the order item attributes.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderItemId Order item ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order item ID parameter.
+   */
+  public getItemAttributes(orderId: number, orderItemId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order item ID parameter.
+    if (!orderItemId || typeof orderItemId !== 'number') {
+      throw new Error('Missing or invalid order item ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${orderId}/items/${orderItemId}/attributes`);
+  }
+
+  /**
+   * Returns the order item properties.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderItemId Order item ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order item ID parameter.
+   */
+  public getItemProperties(orderId: number, orderItemId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order item ID parameter.
+    if (!orderItemId || typeof orderItemId !== 'number') {
+      throw new Error('Missing or invalid order item ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${orderId}/items/${orderItemId}/properties`);
+  }
+
+  /**
+   * Returns a specific order item attribute.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderItemId Order item ID.
+   * @param {Number} orderItemAttributeId Order item attribute ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order item ID parameter.
+   * @throws Will throw error on missing or invalid order item attribute ID parameter.
+   */
+  public getItemAttributeById(orderId: number, orderItemId: number, orderItemAttributeId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order item ID parameter.
+    if (!orderItemId || typeof orderItemId !== 'number') {
+      throw new Error('Missing or invalid order item ID');
+    }
+
+    // Check order item attribute ID parameter.
+    if (!orderItemAttributeId || typeof orderItemAttributeId !== 'number') {
+      throw new Error('Missing or invalid order item attribute ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${orderId}/items/${orderItemId}/attributes/${orderItemAttributeId}`);
+  }
+
+  /**
+   * Returns a specific order item property.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderItemId Order item ID.
+   * @param {Number} orderItemPropertyId Order item property ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order item ID parameter.
+   * @throws Will throw error on missing or invalid order item property ID parameter.
+   */
+  public getItemPropertyById(orderId: number, orderItemId: number, orderItemPropertyId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order item ID parameter.
+    if (!orderItemId || typeof orderItemId !== 'number') {
+      throw new Error('Missing or invalid order item ID');
+    }
+
+    // Check order item property ID parameter.
+    if (!orderItemPropertyId || typeof orderItemPropertyId !== 'number') {
+      throw new Error('Missing or invalid order item property ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${orderId}/items/${orderItemId}/properties/${orderItemPropertyId}`);
+  }
+
+  /**
+   * Returns the order items.
+   * @param {Number} id Order ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid ID parameter.
+   */
+  public getItems(id: number): Promise<ResponseInterface> {
+    // Check ID parameter.
+    if (!id || typeof id !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${id}/items`);
+  }
+
+  /**
+   * Returns an order item.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderItemId Order item ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order item ID parameter.
+   */
+  public getItemById(orderId: number, orderItemId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order item ID parameter.
+    if (!orderItemId || typeof orderItemId !== 'number') {
+      throw new Error('Missing or invalid order item ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${orderId}/items/${orderItemId}`);
+  }
+
+  /**
+   * Returns the order totals.
+   * @param {Number} id Order ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid ID parameter.
+   */
+  public getTotals(id: number): Promise<ResponseInterface> {
+    // Check ID parameter.
+    if (!id || typeof id !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${id}/totals`);
+  }
+
+  /**
+   * Returns an order total.
+   * @param {Number} orderId Order ID.
+   * @param {Number} orderTotalId Order total ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid order ID parameter.
+   * @throws Will throw error on missing or invalid order total ID parameter.
+   */
+  public getTotalById(orderId: number, orderTotalId: number): Promise<ResponseInterface> {
+    // Check order ID parameter.
+    if (!orderId || typeof orderId !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Check order item ID parameter.
+    if (!orderTotalId || typeof orderTotalId !== 'number') {
+      throw new Error('Missing or invalid order total ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${orderId}/items/${orderTotalId}`);
+  }
+
+  /**
+   * Returns all orders.
+   * @param {GetOptionsInterface} [options] GET request modifier options.
+   * @returns {Promise} Request promise.
+   */
+  public get(options?: GetOptionsInterface): Promise<ResponseInterface> {
+    return this.requestDispatcher.get(this.getRoute(), this.parseGetOptions(options));
+  }
+
+  /**
+   * Returns a order.
+   * @param {Number} id Order ID.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid ID parameter.
+   */
+  public getById(id: number): Promise<ResponseInterface> {
+    // Check ID parameter.
+    if (!id || typeof id !== 'number') {
+      throw new Error('Missing or invalid order ID');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(`${this.getRoute()}/${id}`);
+  }
+
+ /**
+   * Searches the orders with the passed term.
+   * @param {String} term Search term.
+   * @returns {Promise} Request promise.
+   * @throws Will throw error on missing or invalid search term parameter.
+   */
+  public search(term: string): Promise<ResponseInterface> {
+    // Check search term parameter.
+    if (!term || typeof term !== 'string') {
+      throw new Error('Missing or invalid search term');
+    }
+
+    // Perform request.
+    return this.requestDispatcher.get(this.getRoute(), { q: term });
   }
 }
 
--- tests/unit/OrderEndpointConnectorTest.js
+++ tests/unit/OrderEndpointConnectorTest.js
@@ -67,6 +67,18 @@ test('OrderEndpointConnector#deleteItemAttribute', t => {
   t.end();
 });
 
+test('OrderEndpointConnector#deleteItemProperty', t => {
+  t.throws(() => instance.deleteItemProperty(), Error, 'Throws error on missing parameters');
+  t.throws(() => instance.deleteItemProperty(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.deleteItemProperty(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.deleteItemProperty(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.throws(() => instance.deleteItemProperty(variables.numbers.positive, variables.numbers.positive), Error, 'Throws error on missing third parameter');
+  t.throws(() => instance.deleteItemProperty(variables.numbers.positive, variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid third parameter');
+  t.doesNotThrow(() => instance.deleteItemProperty(variables.numbers.positive, variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameters');
+  t.ok(instance.deleteItemProperty(variables.numbers.positive, variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
 test('OrderEndpointConnector#deleteTotal', t => {
   t.throws(() => instance.deleteTotal(), Error, 'Throws error on missing parameters');
   t.throws(() => instance.deleteTotal(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
@@ -82,5 +94,128 @@ test('OrderEndpointConnector#delete', t => {
   t.throws(() => instance.delete(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
   t.doesNotThrow(() => instance.delete(variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
   t.ok(instance.delete(variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getHistory', t => {
+  t.throws(() => instance.getHistory(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getHistory(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.doesNotThrow(() => instance.getHistory(variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getHistory(variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getHistoryById', t => {
+  t.throws(() => instance.getHistoryById(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getHistoryById(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getHistoryById(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getHistoryById(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.doesNotThrow(() => instance.getHistoryById(variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getHistoryById(variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getItemAttributes', t => {
+  t.throws(() => instance.getItemAttributes(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getItemAttributes(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getItemAttributes(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getItemAttributes(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.doesNotThrow(() => instance.getItemAttributes(variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getItemAttributes(variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getItemProperties', t => {
+  t.throws(() => instance.getItemProperties(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getItemProperties(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getItemProperties(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getItemProperties(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.doesNotThrow(() => instance.getItemProperties(variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getItemProperties(variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getItemAttributeById', t => {
+  t.throws(() => instance.getItemAttributeById(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getItemAttributeById(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getItemAttributeById(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getItemAttributeById(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.throws(() => instance.getItemAttributeById(variables.numbers.positive, variables.numbers.positive), Error, 'Throws error on missing third parameter');
+  t.throws(() => instance.getItemAttributeById(variables.numbers.positive, variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid third parameter');
+  t.doesNotThrow(() => instance.getItemAttributeById(variables.numbers.positive, variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getItemAttributeById(variables.numbers.positive, variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getItemPropertyById', t => {
+  t.throws(() => instance.getItemPropertyById(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getItemPropertyById(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getItemPropertyById(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getItemPropertyById(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.throws(() => instance.getItemPropertyById(variables.numbers.positive, variables.numbers.positive), Error, 'Throws error on missing third parameter');
+  t.throws(() => instance.getItemPropertyById(variables.numbers.positive, variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid third parameter');
+  t.doesNotThrow(() => instance.getItemPropertyById(variables.numbers.positive, variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getItemPropertyById(variables.numbers.positive, variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getItems', t => {
+  t.throws(() => instance.getItems(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getItems(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getItems(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getItems(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.doesNotThrow(() => instance.getItems(variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getItems(variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getItemById', t => {
+  t.throws(() => instance.getItemById(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getItemById(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getItemById(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getItemById(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.doesNotThrow(() => instance.getItemById(variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getItemById(variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getTotals', t => {
+  t.throws(() => instance.getTotals(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getTotals(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.doesNotThrow(() => instance.getTotals(variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getTotals(variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getTotalById', t => {
+  t.throws(() => instance.getTotalById(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getTotalById(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.throws(() => instance.getTotalById(variables.numbers.positive), Error, 'Throws error on missing second parameter');
+  t.throws(() => instance.getTotalById(variables.numbers.positive, variables.strings.minimal), Error, 'Throws error on invalid second parameter');
+  t.doesNotThrow(() => instance.getTotalById(variables.numbers.positive, variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getTotalById(variables.numbers.positive, variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#get', t => {
+  t.doesNotThrow(() => instance.get().catch(noop), Error, 'Does not throw error on missing parameter');
+  t.doesNotThrow(() => instance.get(variables.strings.minimal).catch(noop), Error, 'Does not throw error when passing invalid parameter');
+  t.ok(instance.get().catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#getById', t => {
+  t.throws(() => instance.getById(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.getById(variables.strings.minimal), Error, 'Throws error when passing invalid parameter');
+  t.doesNotThrow(() => instance.getById(variables.numbers.positive).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.getById(variables.numbers.positive).catch(noop) instanceof Promise, 'Returns a promise');
+  t.end();
+});
+
+test('OrderEndpointConnector#search', t => {
+  t.throws(() => instance.search(), Error, 'Throws error on missing parameter');
+  t.throws(() => instance.search(variables.numbers.positive), Error, 'Throws error on invalid parameter');
+  t.doesNotThrow(() => instance.search(variables.strings.minimal).catch(noop), Error, 'Does not throw error when passing valid parameter');
+  t.ok(instance.search(variables.strings.minimal).catch(noop) instanceof Promise, 'Returns a promise');
   t.end();
 });
