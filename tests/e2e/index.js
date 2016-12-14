43a5cb8197394b86c2ae5afe239b65e8944215a6
r.loyko <r.loyko@gambio.de>
2016-12-14 10:15:00 +0100

    Fixed E2E test issue.

--- tests/e2e/index.js
+++ tests/e2e/index.js
@@ -2,4 +2,4 @@ import CreateClientInstanceScenario from './CreateClientInstanceScenario';
 import CreateGetUpdateSearchDeleteAddressScenario from './CreateGetUpdateSearchDeleteAddressScenario';
 
 CreateClientInstanceScenario()
-  .then(CreateGetUpdateDeleteAddressScenario);
+  .then(CreateGetUpdateSearchDeleteAddressScenario);
