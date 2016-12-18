03c6a72eb786ce3d7293d065acedb6722f161fc5
rx2x <hello@rx2x.net>
2016-12-18 17:26:15 +0100

    FIxed Country E2E test file name.

--- tests/e2e/index.js
+++ tests/e2e/index.js
@@ -1,7 +1,7 @@
 import ClientUsage from './ClientUsage';
 import AddressUsage from './AddressUsage';
 import CategoryUsage from './CategoryUsage';
-import ZoneUsage from './ZoneUsage';
+import CountryUsage from './CountryUsage';
 import CustomerUsage from './CustomerUsage';
 import EmailUsage from './EmailUsage';
 import OrderUsage from './OrderUsage';
@@ -9,7 +9,7 @@ import OrderUsage from './OrderUsage';
 ClientUsage()
   .then(AddressUsage)
   .then(CategoryUsage)
-  .then(ZoneUsage)
+  .then(CountryUsage)
   .then(CustomerUsage)
   .then(EmailUsage)
   .then(OrderUsage);
